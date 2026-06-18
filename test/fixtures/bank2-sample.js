c244f714b5bd9b2e53632235d587e43d094b6908
Anh Van Le <anh@deltastrike.com>
2026-06-18 12:14:10 +1200

    test: real bank-2 OCR fixture

---
+++ test/fixtures/bank2-sample.js
@@ -0,0 +1,29 @@
+// Real OCR from a bank-2 account screen (balances interleaved with signed
+// transactions; multi-line rows: description + account-number + amount).
+module.exports = `
+•ll 2degrees §
+14:52
+Summary
+86%
+MISS A LE
+12-3602-0581571-00
+Transfer
+More options
+>
+SUN 10 MAY 2026
+$234.69
+FC01-0274-0435781-00
+10ofmay
+- $5,000.00
+WED 18 MAR 2026
+$5,234.69
+INVESTMENT BALANCE EX AC
+12-3602-0581571-72
+$5,042.40
+IRD: TAX ON TD INT EX AC
+12-3602-0581571-72
+- $4.96
+TD INTEREST EX AC
+12-3602-0581571-72
++ $47.25
+`;
